df8ef57ab3b973cc6837f6b963dd18609371d6d2
flash <you@example.com>
2018-12-04 19:49:47 +0800

    succ to fetch one news

--- main.go
+++ main.go
@@ -49,8 +49,72 @@ func init_logfile() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 }
 func scratch_one_news(url string) {
+	log.Printf("~~~~~~~~~~~~~~~~~~~~~scratch_one_news : %s" ,url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return;
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return
+	}
+	// Load the HTML document
+	htmlDoc, err := goquery.NewDocumentFromReader(res.Body)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	/*
+		<div class="pg-rail-tall__body" itemprop="articleBody">
+	*/
+
+
+	htmlDoc.Find(".zn-body__paragraph").Each(func(i int, s *goquery.Selection){
+		fmt.Println(s.Text())
+	})
+	txt := htmlDoc.Find(".zn-body__paragraph")
+	article := ""
+	log.Println(txt)
+	for _,n := range txt.Nodes {
+		if n.FirstChild != nil {
+				article += n.FirstChild.Data
+		}
+	}
+	log.Printf("item_uri-------------------->%s " ,article)
+
 
 }
+
+//func translate(article string ) (string, string){
+//	google_web := "https://translate.google.cn/#view=home&op=translate&sl=en&tl=zh-CN&text=";
+//	google_web = google_web + article;
+//	res, err := http.Get(google_web)
+//	if err != nil {
+//		log.Println(err)
+//		return "",""
+//	}
+//	defer res.Body.Close()
+//	if res.StatusCode != 200 {
+//		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+//		return "",""
+//	}
+//
+//	body, err := ioutil.ReadAll(res.Body)
+//	if err != nil {
+//		return "", "翻译出错"
+//	}
+//	// Load the HTML document
+//
+//
+//	return "",""
+//}
+
+
 func scratch_website(url string){
 
 		res, err := http.Get(url)
@@ -86,25 +150,27 @@ func scratch_website(url string){
 
 
 		size := bytes.Count([]byte(destScript),nil)
+		log.Printf("%s page size=%d",gl_url,size)
 		offset := strings.Index(destScript,"contentModel")
-		//log.Println(destScript[offset:(size)]);
+		log.Printf("contentModel offset=%d",offset)
+		//log.Println(destScript[offset:(size-1)]);
 
-		offset_1 := strings.Index(destScript[offset:size],"{")
+		offset_1 := strings.Index(destScript[offset:size-1],"{")
 		offset = offset+offset_1
-		//log.Println(destScript[offset:size]);
+		//log.Println(destScript[offset:size-1]);
 
-		offset_1 = strings.Index(destScript[offset:size],"siblings")
+		offset_1 = strings.Index(destScript[offset:size-1],"siblings")
 		offset = offset+offset_1
-		//log.Println(destScript[offset:size]);
+		//log.Println(destScript[offset:size-1]);
 
-		s := strings.Split(destScript[offset:size],"registryURL:")
+		s := strings.Split(destScript[offset:size-1],"registryURL:")
 		destScript = s[0];
 		size = bytes.Count([]byte(destScript),nil)
 		//log.Println(destScript)
 
 		offset_1 = strings.Index(destScript,"{")
 		offset = offset_1
-		//log.Println(destScript[offset:size]);
+		//log.Println(destScript[offset:size-1]);
 
 		//now got you  "articleList"
 		/*
@@ -120,7 +186,7 @@ func scratch_website(url string){
 
 		*/
 
-		result := gjson.Get(destScript[offset:size], "articleList")
+		result := gjson.Get(destScript[offset:size-1], "articleList")
 		for _, item := range result.Array() {
 			log.Println("----------------")
 			//log.Println(item.String())
@@ -129,9 +195,10 @@ func scratch_website(url string){
 			//item_headline :=item.Get("headline");
 			//item_uri :=item.Get("uri");
 			//item_description :=item.Get("description");
-			log.Printf("item_uri--->%s " ,item_uri.String())
-
-
+			log.Printf("item_uri--->%s " ,gl_url+item_uri.String())
+			log.Printf("item_thumbnail--->%s " ,gl_url+item.Get("thumbnail").String())
+			scratch_one_news(gl_url+item_uri.String())
+			break;
 		}
 
 
